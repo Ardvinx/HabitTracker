023c3ab05e2d052766c7e4ee6058e1842d97430d
ardvin <49413892+Ardvinx@users.noreply.github.com>
2025-07-07 18:33:04 +0200

    Initial commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    hello
+    <div class="card flex justify-center">
+      <Button label="Submit" />
+    </div>
+  </main>
+</template>
