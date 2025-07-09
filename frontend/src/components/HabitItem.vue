b38ca887116ab3b5a792ccbe24344cf2eb167d1e
ardvin <49413892+Ardvinx@users.noreply.github.com>
2025-07-09 22:17:44 +0200

    todo component init

---
+++ frontend/src/components/HabitItem.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-12 w-full border border-white rounded-l">asd</div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HabitItem from '@/components/HabitItem.vue'
+</script>
 
 <template>
-  <div class="card flex justify-center bg-gray-700">
-    <Button label="Submit" />
+  <div class="card flex justify-center w-full">
+    <ul class="w-full max-w-[700px] flex flex-col gap-2">
+      <HabitItem v-for="n in 5" />
+    </ul>
   </div>
 </template>
